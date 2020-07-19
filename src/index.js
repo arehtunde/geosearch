a95475e5122ce4bc0a6cb9a25a5e6dd61f7c21a0
Areh Tunde <arehtunde96@gmail.com>
2020-07-19 21:30:39 +0100

    working on the result page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ let button = document.querySelector('.button');
 let content = document.querySelector('.content');
 let features = document.querySelector('.features');
 let foot = document.querySelector('.foot')
+let main = document.querySelector('.main')
 
-const disp = () => {
+//this help create new element
+const createNode = element => document.createElement(element);
+//this append child to parent
+const append = (parent, child) => parent.append(child);
+
+const initResult = () => {
   content.style.display = 'none';
   features.style.display = 'none';
   foot.style.display = 'none';
 };
 
-button.addEventListener('click', disp);
+const resultPage = () => {
+  initResult()
+  let div = createNode('div');
+  div.className = 'content_logo';
+  append(main, div);
+  div.innerHTML = `<a href="index.html">
+    <i class="fas fa-map-marked-alt"></i>
+    <h1 class="content_logo-header">geo-search</h1>
+    </a>`;
+};
+
+button.addEventListener('click', resultPage);
